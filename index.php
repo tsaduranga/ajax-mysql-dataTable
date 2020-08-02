1fa67c7e9718f11a5c2ed1baa01434a389c21fad
tsaduranga <tsaduranga@gmail.com>
2020-08-02 19:14:37 +0530

    finished , get data from database

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajax Datatable</title>
+    <title>Live Table Data Edit</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
+ 
 <body>
-    
+    <div class="container">
+        <h1 align="center">Ajax Data Table using MySQL and PHP</h1>
+        <br />
+        <div id="disp_data"></div>
+    </div>
 </body>
+ 
 </html>
+<script>
+    $(document).ready(function() {
+        function fetch_data() {
+            $.ajax({
+                url: "select.php",
+                method: "POST",
+                success: function(data) {
+                    $('#disp_data').html(data);
+                }
+            });
+        }
+        fetch_data();
+      
+ 
+   
+    });
+</script> 
---
+++ select.php
@@ -0,0 +1,52 @@
+<?php 
+    $conn = mysqli_connect("localhost", "root", "", "ajaxdata"); 
+    $output = ''; 
+    $sql = "SELECT * FROM students ORDER BY id DESC"; 
+    $result = mysqli_query($conn, $sql); 
+    $output .= ' 
+        <div align="center"> 
+        <table class="table" border="1" bordercolor="#00CCCC"> 
+        <tr> 
+        <th width="10%">Id</th> 
+        <th width="30%">Name</th> 
+        <th width="20%">Email</th> 
+        <th width="40%">Address</th>
+        <th width="10%">Contact No</th>
+        <th width="10%">Edit</th> 
+        <th width="10%">Delete</th>
+            </tr>'; 
+    if(mysqli_num_rows($result) > 0) 
+    { 
+    while($row = mysqli_fetch_array($result)) 
+    { 
+    $output .= ' 
+        <tr> 
+        <td>'.$row["id"].'</td> 
+        <td class="full_name" id="full_name" data-id1="'.$row["id"].'" contenteditable>'.$row["full_name"].'</td> 
+        <td class="email" id="email" data-id2="'.$row["id"].'" contenteditable>'.$row["email"].'</td> 
+        <td class="address" id="address" data-id3="'.$row["id"].'" contenteditable>'.$row["address"].'</td> 
+        <td class="address" id="phone" data-id4="'.$row["id"].'" contenteditable>'.$row["phone"].'</td> 
+        <td><button class="btn btn-warning" type="button" name="edit_btn" data-id4="'.$row["id"].'" id="edit">Edit</button></td>  
+        <td><button type="button" class="btn btn-danger" name="delete_btn" data-id3="'.$row["id"].'" id="delete">Delete</button></td> 
+        </tr> 
+        '; 
+        } 
+ $output .= ' 
+        <tr> 
+        <td></td> 
+        <td id="first_name" contenteditable></td> 
+        <td id="last_name" contenteditable></td> 
+        <td><button type="button" class="btn btn-primary" name="add" id="add">Add</button></td> 
+        </tr> 
+ '; 
+ } 
+ else 
+ { 
+        $output .= '<tr> 
+        <td colspan="4">Data not Found</td> 
+        </tr>'; 
+ } 
+        $output .= '</table> 
+        </div>'; 
+ echo $output; 
+ ?>
